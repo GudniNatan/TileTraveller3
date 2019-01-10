2ee9399b87dc4681ebba3b466f9970766696aeca
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-01-10 11:06:25 +0000

    demo

--- tile.py
+++ tile.py
@@ -1,2 +1,11 @@
 class Tile(object):
-    pass
+    def __init__(self, x: int, y: int, neighbors: dict,
+                 coins: int, win_tile: bool):
+        self.x = x
+        self.y = y
+        self.neighbors = neighbors
+        self.coins = coins
+        self.win_tile = win_tile
+
+    def set_neighbors(self, neighbors):
+        self.neighbors = neighbors
