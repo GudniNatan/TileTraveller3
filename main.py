145235880801428c8e9a6eae2f8e05e766d984b9
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-01-10 12:00:24 +0000

    load game

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ def load_game(filename):
     with open(filename) as a_file:
         file_content = [line.strip() for line in a_file]
     # [width height,[walls], [coins],[playerlocation],[playercoins]]
-    width, height = file_content[0].split()
-    walls = file_content[1]
-    coins = file_content[2]
-    playerlocation = file_content[3]
-    playercoins = file_content[4]
+    width, height = [int(x) for x in file_content[0]]
+    walls = eval(file_content[1])
+    coins = eval(file_content[2])
+    playerlocation = eval(file_content[3])
+    playercoins = int(file_content[4])
+    return Grid(width, height, walls, coins, playerlocation, playercoins)
 
 
 def save_game(filename):
@@ -18,17 +19,17 @@ def save_game(filename):
 
 def main():
     # read game file location and load game
-    filename = input('Enter filename: ')
-    load_game(filename)
-    
+    filename = input('Enter filename: ') 
 
-            
+    grid = load_game(filename)
 
 
     # game loop goes here
     win = False
     while win == False:
         # do something
+        # print lever info
+        
         pass
 
 main()
