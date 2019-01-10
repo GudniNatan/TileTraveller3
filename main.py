5b30009839f68a0911f788bc0c6048a7fc2c1024
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-01-10 10:50:51 +0000

    skeleton

---
+++ main.py
@@ -0,0 +1,18 @@
+from grid import Grid
+
+
+def load_game(filename):
+    pass
+
+
+def save_game(filename):
+    pass
+
+
+def main():
+    # read game file location and load game
+
+    # game loop goes here
+    pass
+
+main()
