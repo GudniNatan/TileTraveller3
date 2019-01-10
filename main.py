d5b45173ab3858e6ae7e286e919d98de420b3670
Hólmfríður Magnea Hákonardóttir <holmfridur@Holmfriurs-MacBook-Pro.local>
2019-01-10 11:10:54 +0000

    reads file

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ def save_game(filename):
 def main():
     # read game file location and load game
     
-    with open('filename..') as a_file:
-        file_content = [[line] for line in a_file]
+    with open('levels/demo.txt') as a_file:
+        file_content = [line.strip() for line in a_file]
+    print(file_content)
     
 
             
